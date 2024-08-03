bec560e80d5dd2cea390ce6374359b2ef23b2f29
BiniyamTafesse <biniyamtafesse228@gmail.com>
2024-08-03 22:38:44 +0300

    GAME

---
+++ EASY/Math/3232. Find if Digit Game Can Be Won.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canAliceWin(self, nums: List[int]) -> bool:
+        single=0
+        double=0
+        for i in nums:
+            if i <=9:
+                single+=i
+            else:
+                double+=i
+        return single != double
