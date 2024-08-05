d815fd1d516db95dd014903a320e7a8a26352bda
Biniyam <biniyamtafesse228@gmail.com>
2024-08-05 12:13:29 +0300

    Sort Colors

--- MEDIUM/Sorting/75. Sort Colors.py
+++ MEDIUM/Sorting/75. Sort Colors.py
@@ -3,15 +3,16 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        low, mid, high = 0, 0, len(nums) - 1
         
-        for i in range(len(nums)):
-            b=i+1
-            while b < len(nums):
-                if nums[b]<nums[i]:
-                    nums[i],nums[b] = nums[b],nums[i]
-                elif nums[i]==nums[b]:
-                    nums[i+1],nums[b] = nums[b],nums[i+1]
-                b+=1
+        while mid <= high:
+            if nums[mid] == 0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+            elif nums[mid] == 1:
+                mid += 1
+            else:  # nums[mid] == 2
+                nums[mid], nums[high] = nums[high], nums[mid]
+                high -= 1
         return nums
-
-
