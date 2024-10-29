6ccd4cc37fc577decb3d2ec50aab188b02079678
BiniyamTafesse <biniyamtafesse228@gmail.com>
2024-10-30 00:54:03 +0300

    pairs

--- MEDIUM/Hash Table/2352. Equal Row and Column Pairs.py
+++ MEDIUM/Hash Table/2352. Equal Row and Column Pairs.py
@@ -5,14 +5,12 @@ class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
         row_count = defaultdict(int)
         
-        # Store rows as tuples in the dictionary
         for row in grid:
             row_count[tuple(row)] += 1
         
         count = 0
         n = len(grid)
         
-        # Check each column
         for j in range(n):
             col = tuple(grid[i][j] for i in range(n))  # Create a tuple for the column
             count += row_count[col]  # Add the count of this column's matches in rows
